0d46be9f874d3d7f9d91ad11d36b34a87052484f
Chad Roberts <chad.roberts@suse.com>
2023-02-06 07:25:57 -0500

    tweak to token fetching

--- get_admin_token.sh
+++ get_admin_token.sh
@@ -1,3 +1,3 @@
 # Optional argument to specify a kubeconfig, otherwise, default is used
 
-KUBECONFIG=$1 kubectl get tokens --template="{{range .items}}{{if and (eq .userPrincipal.loginName \"admin\") (ne .description \"Kubeconfig token\")}}{{.metadata.name}}:{{.token}}{{end}}{{end}}"
+KUBECONFIG=$1 kubectl get tokens --template="{{range .items}}{{if and (eq .userPrincipal.loginName \"admin\") (ne .description \"Kubeconfig token\")}}{{.metadata.name}}:{{.token}} {{end}}{{end}}" | cut -d ' ' -f1
